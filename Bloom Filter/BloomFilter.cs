52dad9b018a7ce1a128d9a765d5c66712002dabb
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2023-12-07 15:32:16 +0100

    Добавьте файлы проекта.

---
+++ Bloom Filter/BloomFilter.cs
@@ -0,0 +1,79 @@
+﻿using Murmur;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bloom_Filter
+{
+    public class BloomFilter
+    {
+        private int m; // size of array
+        private int k; // quantity of hash functions
+        private byte[] arr;
+        public BloomFilter(int n, double p = 0)
+        {
+            this.m = calculateSizeOfArray(n, p);
+            this.k = calculateNumOfHashFumctions(n);
+            arr = new byte[m];
+            for (int i = 0; i < m; i++)
+            {
+                arr[i] = 0; // fill whole array with 0
+            }
+        }
+
+        public void addString(string str)
+        {
+            for(int i = 0; i < k; i++)
+            {
+                int index = hashing(str, i) % m; // calculating modus so it will not go out of bounds
+                if(index < 0) // for negetive numbers
+                {
+                    index += m;
+                }
+                arr[index] = 1;
+            }
+        }
+
+        public bool testString(string str)
+        {
+            for (int i = 0;i < k; i++)
+            {
+                int index = hashing(str, i) % m; // calculating modus so it will not go out of bounds
+                if (index < 0) // for negative numbers
+                {
+                    index += m;
+                }
+                if (arr[index] == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private int calculateSizeOfArray(int n, double p)
+        {
+            return (int) Math.Round(- n * Math.Log(p, 2) / Math.Log(2));
+        }
+
+        private int calculateNumOfHashFumctions(int n)
+        {
+            return (int)((m/(double)n) * Math.Log(2));
+        }
+
+        private int hashing(string str, int seed)
+        {
+            HashAlgorithm murmur = MurmurHash.Create128((uint)seed);
+            byte[] strBytes = Encoding.UTF8.GetBytes(str);
+            byte[] hashArr = murmur.ComputeHash(strBytes);
+            return BitConverter.ToInt32(hashArr, 0);
+        }
+
+        public int M {  get { return m; } }
+        public int K { get { return k; } }
+    }
+}
---
+++ Bloom Filter/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Bloom_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = -1;
+            double p = -1;
+            while (n < 0) // check if user entered right number
+            {
+                Console.Write("Enter n: ");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            while(p < 0 || p > 1) // check if user entered right number
+            {
+                Console.Write("Enter probability: ");
+                p = Convert.ToDouble(Console.ReadLine());
+            }
+            BloomFilter bloomFilter = new BloomFilter(n, p); // creating an object of class
+
+            string file = "words.txt";
+            string[] stringArr = File.ReadAllLines(file); // reading strings from file
+            for (int i = 0; i < stringArr.Length; i++)
+            {
+                bloomFilter.addString(stringArr[i]); // adding them to filter
+            }
+            // testing on a big number of string that are not there
+            Console.WriteLine($"Size of filter: {bloomFilter.M}\nNumber of hash functions: {bloomFilter.K}\nExperemental probability of error: {testBigNumber(bloomFilter), 0:F2}");
+
+            string str = Console.ReadLine();  // added so console will not close immediately in the end 
+        }
+
+        public static double testBigNumber(BloomFilter bloomFilter)
+        {
+            int num = 1000000;
+            int numOfFalse = 0;
+
+            for(int i = 0; i < num; i++)
+            {
+                if (bloomFilter.testString("notinfilter" + i))
+                {
+                    numOfFalse++; // add if wrong classified
+                }
+            }
+
+            return (double)numOfFalse / (double)num;
+        }
+    }
+}
